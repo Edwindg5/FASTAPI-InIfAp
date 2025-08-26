204eb83717d9dfeb3175e324d6823ba5dfa9131b
Edwindg5 <233282@ids.upchiapas.edu.mx>
2025-08-26 00:10:33 -0600

    habilite los cors

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],          # permitir todos los métodos (GET, POST, PUT, DELETE...)
-    allow_headers=["*"],          # permitir todos los headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 # Incluir routers
 app.include_router(analisis_quimicos_router.router)
